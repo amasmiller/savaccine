0ff9653e72448b25458cefb9d96e19298e675a72
Scott Miller <amasmiller@gmail.com>
2021-04-02 13:30:17 -0500

    cleanup

--- index.php
+++ index.php
@@ -223,7 +223,7 @@ Even if 1 million people refresh this site every second, the providers servers s
 <br>
 <b>Is this different than vaccinespotter.org?</b>
 <br>
-Similar concept, but simpler and just for San Antonio. Not affiliated with vaccinespotter.org.
+Similar concept, but simpler and just for San Antonio. Not affiliated with <a href="http://vaccinespotter.org">vaccinespotter.org</a>.
 <br>
 <br>
 <b>Who made this?</b>
